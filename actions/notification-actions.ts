36cae8485ad5784cb60cf475ff277638cff54cd4
Trần Văn Lĩnh <154715486+Tranlinh15@users.noreply.github.com>
2026-01-05 23:18:54 +0700

    Update notification-actions.ts

--- actions/notification-actions.ts
+++ actions/notification-actions.ts
@@ -100,7 +100,7 @@ export async function createJoinRequestNotification(projectId: string) {
         link: `/projects/${projectId}`,
         isRead: false,
         type: "JOIN_REQUEST",
-        requestId: Request.id,
+        //requestId: request.id,
         projectId: project.id,
       },
     });
